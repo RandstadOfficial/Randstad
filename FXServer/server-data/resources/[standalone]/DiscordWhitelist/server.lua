e21bf7d83cd2f54dff1229782b76362bf47eff0c
Marco <dhrmabo@gmail.com>
2020-06-06 18:10:01 +0200

    Fixed discord permissions plugin

--- FXServer/server-data/resources/[standalone]/DiscordWhitelist/server.lua
+++ FXServer/server-data/resources/[standalone]/DiscordWhitelist/server.lua
@@ -2,20 +2,21 @@
 --- Discord Whitelist, Made by FAXES ---
 ----------------------------------------
 
+-- Documentation: https://docs.faxes.zone/docs/discord-whitelist-setup
 --- Config ---
-notWhitelisted = "Helaas... Je bent geen tester voor de server." -- Message displayed when they are not whitelist with the role
-noDiscord = "Je moet discord open hebben staan om de server te joinen." -- Message displayed when discord is not found
+notWhitelistedMessage = "Helaas... Je bent geen burger voor de server." -- Message displayed when they are not whitelist with the role
 
-roles = { -- Role nickname(s) needed to pass the whitelist
-    "Tester",
+whitelistRoles = { -- Role nickname(s) needed to pass the whitelist
+    "539554038971105292",
 }
 
 --- Code ---
 
 AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
     local src = source
+    local passAuth = false
     deferrals.defer()
-    deferrals.update("Checking Permissions")
+    deferrals.update("Checking Permissions...")
 
     for k, v in ipairs(GetPlayerIdentifiers(src)) do
         if string.sub(v, 1, string.len("discord:")) == "discord:" then
@@ -24,14 +25,30 @@ AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
     end
 
     if identifierDiscord then
-        for i = 1, #roles do
-            if exports.discord_perms:IsRolePresent(src, roles[i]) then
-                deferrals.done()
-            else
-                deferrals.done(notWhitelisted)
+        usersRoles = exports.discord_perms:GetRoles(src)
+        local function has_value(table, val)
+            if table then
+                for index, value in ipairs(table) do
+                    if value == val then
+                        return true
+                    end
+                end
+            end
+            return false
+        end
+        for index, valueReq in ipairs(whitelistRoles) do 
+            if has_value(usersRoles, valueReq) then
+                passAuth = true
+            end
+            if next(whitelistRoles,index) == nil then
+                if passAuth == true then
+                    deferrals.done()
+                else
+                    deferrals.done(notWhitelistedMessage)
+                end
             end
         end
     else
-        deferrals.done(noDiscord)
+        deferrals.done("Je moet discord open hebben staan om de server te joinen.")
     end
 end)
