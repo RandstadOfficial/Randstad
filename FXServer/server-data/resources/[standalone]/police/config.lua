29f320e6048e701082ab10e3f419170a32c8be04
Randstad <randstadofficial@gmail.com>
2020-06-07 17:35:05 +0200

    Nieuwe PAUDI

--- FXServer/server-data/resources/[standalone]/police/config.lua
+++ FXServer/server-data/resources/[standalone]/police/config.lua
@@ -138,15 +138,15 @@ Config.Vehicles = {
     ["paudi"] = "Audi A6",
     ["pamarok"] = "Volkswagen Amarok",
     ["pt5"] = "Volkswagen T5",
-    ["pt6"] = "Volkswagen T6",
-    ["pfosprinter"] = "Mercedes-Benz Sprinter (FO)",
+    --["pt6"] = "Volkswagen T6",
+    --["pfosprinter"] = "Mercedes-Benz Sprinter (FO)",
     ["pyamahamotor"] = "Yamaha Motor",
     ["pbmwmotor2"] = "BMW Motor",
     ["pschafter"] = "Unmarked Schafter",
     ["poracle"] = "Unmarked Oracle",
-    ["dsimerc"] = "Mercedes (DSI)",
-    ["dsivito"] = "Mercedes Vito (DSI)",
-    ["dsiq5"] = "Audi Q5 (DSI)",
+    --["dsimerc"] = "Mercedes (DSI)",
+   -- ["dsivito"] = "Mercedes Vito (DSI)",
+    --["dsiq5"] = "Audi Q5 (DSI)",
 }
 
 Config.AmmoLabels = {
