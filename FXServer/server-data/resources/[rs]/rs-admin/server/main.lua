ae26641833a13185ce9d125770e613b3327ca426
Marco <dhrmabo@gmail.com>
2020-06-13 20:17:10 +0200

    added /cleararea command to remove peds

--- FXServer/server-data/resources/[rs]/rs-admin/server/main.lua
+++ FXServer/server-data/resources/[rs]/rs-admin/server/main.lua
@@ -203,6 +203,10 @@ RSCore.Commands.Add("reporttoggle", "Toggle inkomende reports uit of aan", {}, f
     end
 end, "admin")
 
+RSCore.Commands.Add("cleararea", "verwijder peds, voertuigen en alles in het gebied", {}, false, function(source, args)
+    TriggerClientEvent('RSCore:ClearArea', -1)
+end, "admin")
+
 RegisterCommand("kickall", function(source, args, rawCommand)
     local src = source
     
@@ -251,12 +255,7 @@ end)
 
 RegisterServerEvent('rs-admin:server:setPermissions')
 AddEventHandler('rs-admin:server:setPermissions', function()
-    local reason = "Doei doei hackertje"
-    local banTime = 2147483647
-    local timeTable = os.date("*t", banTime)
-    TriggerClientEvent('chatMessage', -1, "BANHAMMER", "error", GetPlayerName(source).." is verbannen voor: "..reason.."")
-    RSCore.Functions.ExecuteSql(false, "INSERT INTO `bans` (`name`, `steam`, `license`, `discord`,`ip`, `reason`, `expire`) VALUES ('"..GetPlayerName(source).."', '"..GetPlayerIdentifiers(source)[1].."', '"..GetPlayerIdentifiers(source)[2].."', '"..GetPlayerIdentifiers(source)[3].."', '"..GetPlayerIdentifiers(source)[4].."', '"..reason.."', "..banTime..")")
-    DropPlayer(source, "Hé sukkel, je bent verbannen van de server:\n"..reason.."\n\nJe ban verloopt "..timeTable["day"].. "/" .. timeTable["month"] .. "/" .. timeTable["year"] .. " " .. timeTable["hour"].. ":" .. timeTable["min"] .. "\n🔸 Kijk op onze discord voor meer informatie")
+    RSCore.Functions.BanInjection(source)
 end)
 
 RSCore.Functions.CreateCallback('rs-admin:setPermissions', function(source, cb, targetId, group)
