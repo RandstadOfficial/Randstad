b2598be03dc7c72a84bc7212edaa7a1c09552c72
Marco <dhrmabo@gmail.com>
2020-06-24 22:16:41 +0200

    setup first node modules

---
+++ FXServer/server-data/resources/[rs]/rs-bot/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- new resource manifest https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/
+fx_version 'bodacious'
+-- https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/#fx-version-bodacious-2020-02
+games {"gta5"}
+
+author "MaBo"
+description "FiveM Discord bot"
+version "1.0.0"
+
+server_script "bot.js"
