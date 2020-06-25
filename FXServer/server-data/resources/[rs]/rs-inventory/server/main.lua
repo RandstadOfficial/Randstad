42456609839f9e39bd06172d84302e6849fab0ef
Nicohaverdil <nhv.haverdil@hotmail.com>
2020-06-25 15:40:14 +0200

    removes prints

--- FXServer/server-data/resources/[rs]/rs-inventory/server/main.lua
+++ FXServer/server-data/resources/[rs]/rs-inventory/server/main.lua
@@ -66,9 +66,6 @@ AddEventHandler('inventory:server:OpenInventory', function(name, id, other)
 			secondInv.inventory = {}
 			secondInv.slots = slots
 			if Stashes[id] ~= nil and Stashes[id].isOpen then
-				print("I am trying to open a nil or open stash")
-				print(Stashes[id])
-				print(Stashes[id].isOpen)
 				secondInv.name = "none-inv"
 				secondInv.label = "Stash-None"
 				secondInv.maxweight = 1000000
