fb77e0ee37af13c686a6beb7eef933f244e99464
Nicohaverdil <nhv.haverdil@hotmail.com>
2020-06-26 11:10:59 +0200

    Adds ragdoll when sprint jumping

--- FXServer/server-data/resources/[standalone]/animations/client/Ragdoll.lua
+++ FXServer/server-data/resources/[standalone]/animations/client/Ragdoll.lua
@@ -23,3 +23,24 @@ Citizen.CreateThread(function()
   end
 end)
 
+-- Ragdoll when sprint jumping.
+local ragdoll_chance = 0.5 -- edit this decimal value for chance of falling (e.g. 80% = 0.8    75% = 0.75    32% = 0.32)
+
+-- code, not recommended to edit below this point
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(100) -- check every 100 ticks, performance matters
+		local ped = PlayerPedId()
+		if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
+			local chance_result = math.random()
+			if chance_result < ragdoll_chance then 
+				Citizen.Wait(600) -- roughly when the ped loses grip
+                ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.08) -- change this float to increase/decrease camera shake
+				SetPedToRagdoll(ped, 5000, 1, 2)
+			else
+				Citizen.Wait(2000) -- cooldown before continuing
+			end
+		end
+	end
+end)
+
