656a9119427755fb00bbbd6db40a063dd009d7d1
Nicohaverdil <nhv.haverdil@hotmail.com>
2020-06-26 13:00:39 +0200

    Fixes ctrl bug completely

--- FXServer/server-data/resources/[rs]/rs-smallresources/client/crouchprone.lua
+++ FXServer/server-data/resources/[rs]/rs-smallresources/client/crouchprone.lua
@@ -1,33 +1,37 @@
 local stage = 0
 local movingForward = false
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
-        if not IsPedSittingInAnyVehicle(GetPlayerPed(-1)) then
+        local ped = GetPlayerPed(-1)
+        if not IsPedSittingInAnyVehicle(ped) and IsPedOnFoot(ped) and not IsPedFalling(ped) then
             if IsControlJustReleased(0, Keys["LEFTCTRL"]) then
-                stage = stage + 1
-                if stage == 2 then
-                    -- Crouch stuff
-                    ClearPedTasks(GetPlayerPed(-1))
-                    RequestAnimSet("move_ped_crouched")
-                    while not HasAnimSetLoaded("move_ped_crouched") do
-                        Citizen.Wait(0)
-                    end
+                if not isPedDoingAnimations() then
+                    stage = stage + 1
+                    if stage == 2 then
+                        -- Crouch stuff
+                        ClearPedTasks(GetPlayerPed(-1))
+                        RequestAnimSet("move_ped_crouched")
+                        while not HasAnimSetLoaded("move_ped_crouched") do
+                            Citizen.Wait(0)
+                        end
 
-                    SetPedMovementClipset(GetPlayerPed(-1), "move_ped_crouched",1.0)    
-                    SetPedWeaponMovementClipset(GetPlayerPed(-1), "move_ped_crouched",1.0)
-                    SetPedStrafeClipset(GetPlayerPed(-1), "move_ped_crouched_strafing",1.0)
-                elseif stage == 3 then
-                    ClearPedTasks(GetPlayerPed(-1))
-                    RequestAnimSet("move_crawl")
-                    while not HasAnimSetLoaded("move_crawl") do
-                        Citizen.Wait(0)
+                        SetPedMovementClipset(GetPlayerPed(-1), "move_ped_crouched",1.0)    
+                        SetPedWeaponMovementClipset(GetPlayerPed(-1), "move_ped_crouched",1.0)
+                        SetPedStrafeClipset(GetPlayerPed(-1), "move_ped_crouched_strafing",1.0)
+                    elseif stage == 3 then
+                        ClearPedTasks(GetPlayerPed(-1))
+                        RequestAnimSet("move_crawl")
+                        while not HasAnimSetLoaded("move_crawl") do
+                            Citizen.Wait(0)
+                        end
+                    elseif stage > 3 then
+                        stage = 0
+                        ClearPedTasksImmediately(GetPlayerPed(-1))
+                        ResetAnimSet()
+                        SetPedStealthMovement(GetPlayerPed(-1),0,0)
                     end
-                elseif stage > 3 then
-                    stage = 0
-                    ClearPedTasksImmediately(GetPlayerPed(-1))
-                    ResetAnimSet()
-                    SetPedStealthMovement(GetPlayerPed(-1),0,0)
                 end
             end
 
@@ -68,6 +72,7 @@ Citizen.CreateThread(function()
             end
         else
             stage = 0
+            ped = PlayerPedId()
             Citizen.Wait(1000)
         end
     end
@@ -78,6 +83,39 @@ RegisterNetEvent("crouchprone:client:SetWalkSet")
 AddEventHandler("crouchprone:client:SetWalkSet", function(clipset)
     walkSet = clipset
 end)
+
+--Not in table because lazy
+function isPedDoingAnimations()
+    print("Checking for anim")
+    local ped = GetPlayerPed(-1)
+
+    if IsPedUsingAnyScenario(ped) then
+        print("am doing scenario")
+        return true
+    end
+    if IsEntityPlayingAnim(ped, "mp_car_bomb", "car_bomb_mechanic", 3) then
+        print("am doing mechanic anim")
+        return true
+    end
+    if IsEntityPlayingAnim(ped, "anim@heists@box_carry@", "idle", 3) then
+        print("am doing idle anim")
+        return true
+    end
+    if IsEntityPlayingAnim(ped, 'anim@heists@fleeca_bank@drilling', 'drill_straight_idle', 3) then
+        print("am doing drilling anim")
+        return true
+    end
+    if IsEntityPlayingAnim(ped, "mini@safe_cracking", "dial_turn_anti_fast_1", 3) then
+        print("am doing safe cracking anim")
+        return true
+    end
+    if IsEntityPlayingAnim(ped, "anim@gangops@facility@servers@", "hotwire", 3) then
+        print("am doing hotwire anim")
+        return true
+    end
+    print("Was not doing anim")
+    return false;
+end
 
 
 function ResetAnimSet()
