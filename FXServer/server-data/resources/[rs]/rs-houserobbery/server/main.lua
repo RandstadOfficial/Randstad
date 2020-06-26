370cfbaeca106ebdb7f8f44d4783bd561278c028
Nicohaverdil <nhv.haverdil@hotmail.com>
2020-06-26 15:48:15 +0200

    Adds explanation for Maestro, Adds rare loot table

--- FXServer/server-data/resources/[rs]/rs-houserobbery/server/main.lua
+++ FXServer/server-data/resources/[rs]/rs-houserobbery/server/main.lua
@@ -34,36 +34,46 @@ function ResetHouseStateTimer(house)
     end)
 end
 
+local rareLoot = {
+    "weapon_bat",
+    "painkiller"
+}
+
+function pickSpecialReward()
+    return rareLoot[math.random(1, #rareLoot)]
+end
+
 RegisterServerEvent('rs-houserobbery:server:searchCabin')
 AddEventHandler('rs-houserobbery:server:searchCabin', function(cabin, house)
     local src = source
     local Player = RSCore.Functions.GetPlayer(src)
     local luck = math.random(1, 10)
-    local itemFound = math.random(1, 4)
+    local itemFound = math.random(1, 4) -- Chance of items should be 75%, 25% failure. If it is 4, failure.
     local itemCount = 1
 
     if itemFound < 4 then
-        if luck == 10 then
-            itemCount = 3
-        elseif luck >= 6 and luck <= 8 then
+        if luck == 10 then -- 10% chance on 3 items, want more? buff below
+            itemCount = 3 
+        elseif luck >= 6 and luck <= 8 then -- 30% chance on 2 items, want more? buff below
             itemCount = 2
         end
+        -- So theres 60% chance you only get 1 item, because itemCount is 1 by default.
 
         for i = 1, itemCount, 1 do
             local randomItem = Config.Rewards[Config.Houses[house]["furniture"][cabin]["type"]][math.random(1, #Config.Rewards[Config.Houses[house]["furniture"][cabin]["type"]])]
             local itemInfo = RSCore.Shared.Items[randomItem]
-            if math.random(1, 100) == 69 then
-                randomItem = "painkillers"
+            if math.random(1, 100) == 69 then -- 1% chance to get painkillers as item per itemcount
+                randomItem = pickSpecialReward()
                 itemInfo = RSCore.Shared.Items[randomItem]
                 Player.Functions.AddItem(randomItem, 1)
                 TriggerClientEvent('inventory:client:ItemBox', src, itemInfo, "add")
             else
                 if not itemInfo["unqiue"] then
-                    local itemAmount = math.random(1, 3)
+                    local itemAmount = math.random(1, 3) -- 1 to 3 diamond rings, rolex, tosti, sandwich
                     if randomItem == "plastic" then
-                        itemAmount = math.random(15, 20)
+                        itemAmount = math.random(15, 20) -- 15 to 20 pieces of plastic
                     elseif randomItem == "goldchain" then
-                        itemAmount = math.random(2, 5)
+                        itemAmount = math.random(2, 5) -- 2 to 5 pieces of goldchain
                     end
                     Player.Functions.AddItem(randomItem, itemAmount)
                     --TriggerClientEvent('RSCore:Notify', src, '+'..itemAmount..' '..itemInfo["label"], 'success', 3500)
@@ -73,7 +83,6 @@ AddEventHandler('rs-houserobbery:server:searchCabin', function(cabin, house)
                 TriggerClientEvent('inventory:client:ItemBox', src, itemInfo, "add")
             end
             Citizen.Wait(500)
-            -- local weaponChance = math.random(1, 100)
         end
     else
         TriggerClientEvent('RSCore:Notify', src, 'Het kastje is leeg broooo', 'error', 3500)
