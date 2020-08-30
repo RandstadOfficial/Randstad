e7f8bf8d4bafbc2fb3508c0f4996f54645d881d6
Maestro <maesttro27@gmail.com>
2020-08-30 14:39:48 +0200

    Lunk Banking (plofkraken en atm systeem)

--- FXServer/server-data/resources/[standalone]/banking/client.lua
+++ FXServer/server-data/resources/[standalone]/banking/client.lua
@@ -24,90 +24,17 @@ end)
 -- Code
 
 -- Settings
-local depositAtATM = false -- Allows the player to deposit at an ATM rather than only in banks (Default: false)
+local depositAtATM = true -- Allows the player to deposit at an ATM rather than only in banks (Default: false)
 local giveCashAnywhere = false -- Allows the player to give CASH to another player, no matter how far away they are. (Default: false)
 local withdrawAnywhere = false -- Allows the player to withdraw cash from bank account anywhere (Default: false)
 local depositAnywhere = false -- Allows the player to deposit cash into bank account anywhere (Default: false)
 local displayBankBlips = true -- Toggles Bank Blips on the map (Default: true)
-local displayAtmBlips = false -- Toggles ATM blips on the map (Default: false) // THIS IS UGLY. SOME ICONS OVERLAP BECAUSE SOME PLACES HAVE MULTIPLE ATM MACHINES. NOT RECOMMENDED
+local displayAtmBlips = true -- Toggles ATM blips on the map (Default: false) // THIS IS UGLY. SOME ICONS OVERLAP BECAUSE SOME PLACES HAVE MULTIPLE ATM MACHINES. NOT RECOMMENDED
 local enableBankingGui = true -- Enables the banking GUI (Default: true) // MAY HAVE SOME ISSUES
 
 -- ATMS
-local atms = {
-  {name="ATM", id=277, x=-386.733, y=6045.953, z=31.501},
-  {name="ATM", id=277, x=-284.037, y=6224.385, z=31.187},
-  {name="ATM", id=277, x=-284.037, y=6224.385, z=31.187},
-  {name="ATM", id=277, x=-135.165, y=6365.738, z=31.101},
-  {name="ATM", id=277, x=-110.753, y=6467.703, z=31.784},
-  {name="ATM", id=277, x=-94.9690, y=6455.301, z=31.784},
-  {name="ATM", id=277, x=155.4300, y=6641.991, z=31.784},
-  {name="ATM", id=277, x=174.6720, y=6637.218, z=31.784},
-  {name="ATM", id=277, x=1703.138, y=6426.783, z=32.730},
-  {name="ATM", id=277, x=1735.114, y=6411.035, z=35.164},
-  {name="ATM", id=277, x=1702.842, y=4933.593, z=42.051},
-  {name="ATM", id=277, x=1967.333, y=3744.293, z=32.272},
-  {name="ATM", id=277, x=1821.917, y=3683.483, z=34.244},
-  {name="ATM", id=277, x=1174.532, y=2705.278, z=38.027},
-  {name="ATM", id=277, x=540.0420, y=2671.007, z=42.177},
-  {name="ATM", id=277, x=2564.399, y=2585.100, z=38.016},
-  {name="ATM", id=277, x=2558.683, y=349.6010, z=108.050},
-  {name="ATM", id=277, x=2558.051, y=389.4817, z=108.660},
-  {name="ATM", id=277, x=1077.692, y=-775.796, z=58.218},
-  {name="ATM", id=277, x=1139.018, y=-469.886, z=66.789},
-  {name="ATM", id=277, x=1168.975, y=-457.241, z=66.641},
-  {name="ATM", id=277, x=1153.884, y=-326.540, z=69.245},
-  {name="ATM", id=277, x=381.2827, y=323.2518, z=103.270},
-  {name="ATM", id=277, x=236.4638, y=217.4718, z=106.840},
-  {name="ATM", id=277, x=265.0043, y=212.1717, z=106.780},
-  {name="ATM", id=277, x=285.2029, y=143.5690, z=104.970},
-  {name="ATM", id=277, x=157.7698, y=233.5450, z=106.450},
-  {name="ATM", id=277, x=-164.568, y=233.5066, z=94.919},
-  {name="ATM", id=277, x=-1827.04, y=785.5159, z=138.020},
-  {name="ATM", id=277, x=-1409.39, y=-99.2603, z=52.473},
-  {name="ATM", id=277, x=-1205.35, y=-325.579, z=37.870},
-  {name="ATM", id=277, x=-1215.64, y=-332.231, z=37.881},
-  {name="ATM", id=277, x=-2072.41, y=-316.959, z=13.345},
-  {name="ATM", id=277, x=-2975.72, y=379.7737, z=14.992},
-  {name="ATM", id=277, x=-2962.60, y=482.1914, z=15.762},
-  {name="ATM", id=277, x=-2955.70, y=488.7218, z=15.486},
-  {name="ATM", id=277, x=-3044.22, y=595.2429, z=7.595},
-  {name="ATM", id=277, x=-3144.13, y=1127.415, z=20.868},
-  {name="ATM", id=277, x=-3241.10, y=996.6881, z=12.500},
-  {name="ATM", id=277, x=-3241.11, y=1009.152, z=12.877},
-  {name="ATM", id=277, x=-1305.40, y=-706.240, z=25.352},
-  {name="ATM", id=277, x=-538.225, y=-854.423, z=29.234},
-  {name="ATM", id=277, x=-711.156, y=-818.958, z=23.768},
-  {name="ATM", id=277, x=-717.614, y=-915.880, z=19.268},
-  {name="ATM", id=277, x=-526.566, y=-1222.90, z=18.434},
-  {name="ATM", id=277, x=-256.831, y=-719.646, z=33.444},
-  {name="ATM", id=277, x=-203.548, y=-861.588, z=30.205},
-  {name="ATM", id=277, x=112.4102, y=-776.162, z=31.427},
-  {name="ATM", id=277, x=112.9290, y=-818.710, z=31.386},
-  {name="ATM", id=277, x=119.9000, y=-883.826, z=31.191},
-  {name="ATM", id=277, x=149.4551, y=-1038.95, z=29.366},
-  {name="ATM", id=277, x=-846.304, y=-340.402, z=38.687},
-  {name="ATM", id=277, x=-1204.35, y=-324.391, z=37.877},
-  {name="ATM", id=277, x=-1216.27, y=-331.461, z=37.773},
-  {name="ATM", id=277, x=-56.1935, y=-1752.53, z=29.452},
-  {name="ATM", id=277, x=-261.692, y=-2012.64, z=30.121},
-  {name="ATM", id=277, x=-273.001, y=-2025.60, z=30.197},
-  {name="ATM", id=277, x=314.187, y=-278.621, z=54.170},
-  {name="ATM", id=277, x=-351.534, y=-49.529, z=49.042},
-  {name="ATM", id=277, x=24.589, y=-946.056, z=29.357},
-  {name="ATM", id=277, x=-254.112, y=-692.483, z=33.616},
-  {name="ATM", id=277, x=-1570.197, y=-546.651, z=34.955},
-  {name="ATM", id=277, x=-1415.909, y=-211.825, z=46.500},
-  {name="ATM", id=277, x=-1430.112, y=-211.014, z=46.500},
-  {name="ATM", id=277, x=33.232, y=-1347.849, z=29.497},
-  {name="ATM", id=277, x=129.216, y=-1292.347, z=29.269},
-  {name="ATM", id=277, x=287.645, y=-1282.646, z=29.659},
-  {name="ATM", id=277, x=289.012, y=-1256.545, z=29.440},
-  {name="ATM", id=277, x=295.839, y=-895.640, z=29.217},
-  {name="ATM", id=277, x=1686.753, y=4815.809, z=42.008},
-  {name="ATM", id=277, x=-302.408, y=-829.945, z=32.417},
-  {name="ATM", id=277, x=5.134, y=-919.949, z=29.557},
-
-}
+local atms = nil
+local CurrentCops = 0
 
 ATMObjects = {
   -870868698,
@@ -129,13 +56,30 @@ local banks = {
   [9] = {name="Bank", Closed = false, id=108, x = 1175.21,  y = 2706.517, z = 38.09},
 }
 
+AddEventHandler('onClientGameTypeStart', function()  --LLG
+  TriggerServerEvent('rs-banking:server:GetAtms')
+end)
+
+RegisterNetEvent('rs-banking:client:GetAtms')
+AddEventHandler('rs-banking:client:GetAtms', function(result)
+  atms = nil
+  atms = result
+end)
+
 RegisterNetEvent('rs-banking:client:SetBankClosed')
 AddEventHandler('rs-banking:client:SetBankClosed', function(BankId, bool)
   banks[BankId].Closed = bool
 end)
 
+RegisterNetEvent('police:SetCopCount')
+AddEventHandler('police:SetCopCount', function(amount)
+    CurrentCops = amount
+end)
+
+
 -- Display Map Blips
 Citizen.CreateThread(function()
+  TriggerServerEvent('rs-banking:server:GetAtms')
   if (displayBankBlips == true) then
     for _, item in pairs(banks) do
         item.blip = AddBlipForCoord(item.x, item.y, item.z)
@@ -149,14 +93,18 @@ Citizen.CreateThread(function()
     end
   end
 
+  while atms == nil do --LLG
+    Citizen.Wait(100)
+  end
+
   if (displayAtmBlips == true) then
-    for _, item in pairs(atms) do
+    for _, item in pairs(atms) do -- Added hardcoded name ATM and blipSprite 277
       item.blip = AddBlipForCoord(item.x, item.y, item.z)
-      SetBlipSprite(item.blip, item.id)
+      SetBlipSprite(item.blip, 277)
       SetBlipScale(item.blip, 0.65)
       SetBlipAsShortRange(item.blip, true)
       BeginTextCommandSetBlipName("STRING")
-      AddTextComponentString(item.name)
+      AddTextComponentString('ATM')
       EndTextCommandSetBlipName(item.blip)
     end
   end
@@ -210,57 +158,276 @@ DrawText3Ds = function(x, y, z, text)
     ClearDrawOrigin()
 end
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(3)
+function SpawnMoney(pos) --LLG
+  local prop = "prop_anim_cash_pile_02"
+  Citizen.CreateThread(function()
+    RequestModel(prop)
+    while not HasModelLoaded(prop) do
+      Citizen.Wait(100)
+    end
+    local amount = math.random(5, 15)
+    while amount >= 0 do
+      local object = CreateObject(prop, pos.x, pos.y, pos.z, true, true, true)
+      PlaceObjectOnGroundProperly(object)
+      amount = amount - 1
+    end
+  end)
+end
 
-        inRange = false
+function PlaceExplosive(pos) --LLG
+  Citizen.CreateThread(function()
+    local Chance = math.random(1, 20)
+    --print(Chance)
+    if Chance == 5 then
+      Citizen.Wait(1500)
+      AddExplosion(pos.x, pos.y, pos.z, EXPLOSION_GAS_TANK, 1.0, true, false, 1.0)
+      SpawnMoney(pos)
+    else
+      Citizen.Wait(15000)
+      AddExplosion(pos.x, pos.y, pos.z, EXPLOSION_GAS_TANK, 1.0, true, false, 1.0)
+      SpawnMoney(pos)
+    end
+  end)
+end
 
-        local pos = GetEntityCoords(GetPlayerPed(-1))
-        local nearbank, bankkey = IsNearBank()
-        
-        if nearbank then
-          atBank = true
-          inRange = true
-          if not banks[bankkey].Closed then
-            DrawMarker(2, banks[bankkey].x, banks[bankkey].y, banks[bankkey].z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 0.1, 55, 255, 55, 255, 0, 0, 0, 1, 0, 0, 0)
-            DrawText3Ds(banks[bankkey].x, banks[bankkey].y, banks[bankkey].z + 0.3, '[E] Kaart valideren')
-            if IsControlJustPressed(1, Keys["E"])  then
-                if (not IsInVehicle()) then
-                    if bankOpen then
-                        closeGui()
-                        bankOpen = false
-                    else
-                        openGui()
-                        bankOpen = true
-                    end
-                end
-            end
-          else
-            DrawText3Ds(banks[bankkey].x, banks[bankkey].y, banks[bankkey].z + 0.3, 'De bank is gesloten')
-            DrawMarker(2, banks[bankkey].x, banks[bankkey].y, banks[bankkey].z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 0.1, 255, 55, 55, 255, 0, 0, 0, 1, 0, 0, 0)
-          end
-        elseif IsNearATM() then
-            atBank = true
-            inRange = true
-            DrawText3Ds(pos.x, pos.y, pos.z, '[E] Kaart valideren')
-            if IsControlJustPressed(1, Keys["E"])  then
-              if (not IsInVehicle()) then
-                    if bankOpen then
-                        closeGui()
-                        bankOpen = false
-                    else
-                        openGui()
-                        bankOpen = true
-                    end
-                end
-            end
-        end
+function GetAtmFromDB(pos)  --LLG
+  if atms == nil or pos == nil then
+    return nil
+  end
+  for _,atm in pairs(atms) do
+    local dist = GetDistanceBetweenCoords(pos.x, pos.y, pos.z, atm.x, atm.y, atm.z, true)
+    if dist <= 7.5 then
+      return atm.id
+    end
+  end
+end
 
-        if not inRange then
-          Citizen.Wait(1500)
+function removeCash()
+  Citizen.CreateThread(function()
+    local prop = "prop_anim_cash_pile_02"
+    local strike = 0
+    local amount = 0
+    while true and strike < 10 do
+      local pedCoords = GetEntityCoords(PlayerPedId())
+      local objectId = GetClosestObjectOfType(pedCoords, 5.0, GetHashKey(prop), false)
+      if objectId ~= 0 then
+        DeleteEntity(objectId)
+        DeleteObject(objectId)
+        amount = amount + 1
+      else
+        strike = strike + 1
+      end
+      Citizen.Wait(250)
+    end
+  end)
+end
+
+function PoliceAlert()
+  local ped = GetPlayerPed(-1)
+  local pos = GetEntityCoords(ped)
+  local s1, s2 = Citizen.InvokeNative(0x2EB41072B4C1E4C0, pos.x, pos.y, pos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
+  local street1 = GetStreetNameFromHashKey(s1)
+  local street2 = GetStreetNameFromHashKey(s2)
+  local streetLabel = street1
+  if street2 ~= nil then 
+      streetLabel = streetLabel .. " " .. street2
+  end
+
+  TriggerServerEvent('banking:server:callCops', streetLabel, pos)
+end
+
+RegisterNetEvent('banking:client:robberyCall')
+AddEventHandler('banking:client:robberyCall', function(msg, streetLabel, coords)
+	PlaySound(-1, "Lose_1st", "GTAO_FM_Events_Soundset", 0, 0, 1)
+	TriggerEvent('rs-policealerts:client:AddPoliceAlert', {
+		timeOut = 5000,
+		alertTitle = "Plofkraak",
+		coords = {
+			x = coords.x,
+			y = coords.y,
+			z = coords.z,
+		},
+		details = {
+			[1] = {
+				icon = '<i class="far fa-credit-card"></i>',
+				detail = " Geldautomaat",
+			},
+			[2] = {
+				icon = '<i class="fas fa-globe-europe"></i>',
+				detail = streetLabel,
+			},
+		},
+		callSign = RSCore.Functions.GetPlayerData().metadata["callsign"],
+	})
+    local transG = 250
+    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+    SetBlipSprite(blip, 458)
+    SetBlipColour(blip, 1)
+    SetBlipDisplay(blip, 4)
+    SetBlipAlpha(blip, transG)
+    SetBlipScale(blip, 1.0)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Plofkraak")
+    EndTextCommandSetBlipName(blip)
+    while transG ~= 0 do
+        Wait(180 * 4)
+        transG = transG - 1
+        SetBlipAlpha(blip, transG)
+        if transG == 0 then
+            SetBlipSprite(blip, 2)
+            RemoveBlip(blip)
+            return
         end
     end
+end)
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(3)
+
+    inRange = false
+
+    local pos = GetEntityCoords(GetPlayerPed(-1))
+    local nearbank, bankkey = IsNearBank()
+    local atmId = GetAtmFromDB(pos)
+
+    if nearbank then
+      atBank = true
+      inRange = true
+      if not banks[bankkey].Closed then
+        DrawMarker(2, banks[bankkey].x, banks[bankkey].y, banks[bankkey].z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 0.1, 55, 255, 55, 255, 0, 0, 0, 1, 0, 0, 0)
+        DrawText3Ds(banks[bankkey].x, banks[bankkey].y, banks[bankkey].z + 0.3, '[E] Kaart valideren')
+        if IsControlJustPressed(1, Keys["E"])  then
+          if (not IsInVehicle()) then
+            if bankOpen then
+              closeGui()
+              bankOpen = false
+            else
+              openGui()
+              bankOpen = true
+            end
+          end
+        end
+      else
+        DrawText3Ds(banks[bankkey].x, banks[bankkey].y, banks[bankkey].z + 0.3, 'De bank is gesloten')
+        DrawMarker(2, banks[bankkey].x, banks[bankkey].y, banks[bankkey].z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2, 0.1, 255, 55, 55, 255, 0, 0, 0, 1, 0, 0, 0)
+      end
+    elseif IsNearATM() then
+      atBank = true
+      inRange = true
+      if atms ~= nil then
+        --print(atmId) -- dit is om ATM id te zien
+        if atms[atmId] ~= nil then
+          if atms[atmId].blocked == 0 and atms[atmId].cashInside > 0 then
+            if atms[atmId].isHijacked == 0 then
+              DrawText3Ds(pos.x, pos.y, pos.z, '[E] Kaart valideren')
+              if atms[atmId].hijackable == 1 then
+              DrawText3Ds(pos.x, pos.y, pos.z-0.15, '[R] Om een plofkraak te plegen')
+              end
+            else
+              if atms[atmId].hijackable == 1 then
+                DrawText3Ds(pos.x, pos.y, pos.z, '[R] Om geld op te pakken')
+              end
+            end
+
+            DisableControlAction(0, 140, true) --LLG
+
+            if IsDisabledControlJustPressed(0, 140) and atms[atmId].hijackable == 1 then
+              if atms[atmId].isHijacked == 0 then
+                if CurrentCops >= 0 then
+                  RSCore.Functions.TriggerCallback('RSCore:HasItem', function(result)
+                    if result then 
+                      RSCore.Functions.Progressbar("", "Gasbom plaatsen...", math.random(7500, 15000), false, true, {
+                        disableMovement = true,
+                        disableCarMovement = true,
+                        disableMouse = false,
+                        disableCombat = true,
+                      }, {
+                        animDict = "anim@gangops@facility@servers@",
+                        anim = "hotwire",
+                        flags = 16,
+                      }, {}, {}, function() -- Done
+                        PlaceExplosive(pos)
+                        PoliceAlert()
+                        StopAnimTask(GetPlayerPed(-1), "anim@gangops@facility@servers@", "hotwire", 1.0)
+                        local data = {}
+                        data.isHijacked = 1
+                        TriggerServerEvent('rs-banking:server:UpdateATM', atmId, data)
+                        TriggerServerEvent("RSCore:Server:RemoveItem", "gasbomb", 1)
+                        TriggerEvent("inventory:client:ItemBox", RSCore.Shared.Items["gasbomb"], "remove")
+                        RSCore.Functions.Notify("Gasbom geplaatst, wacht tot die afgaat...", "success")
+                        ClearPedTasksImmediately(ped)
+                      end, function() -- Cancel
+                        ClearPedTasksImmediately(ped)
+                        StopAnimTask(GetPlayerPed(-1), "anim@gangops@facility@servers@", "hotwire", 1.0)
+                        RSCore.Functions.Notify("Geannuleerd..", "error")
+                      end)
+                    else
+                        RSCore.Functions.Notify("Je mist een item..", "error")
+                    end
+                  end, "gasbomb")
+                  Citizen.Wait(1000)
+                else
+                  RSCore.Functions.Notify('Niet genoeg agenten..', 'error', 3500)
+                end
+              else
+                removeCash()
+                local earning = math.random(3000, 6000)
+                RSCore.Functions.Progressbar("take_atm_money", "Geld Pakken...", 15000, false, true, {
+                  disableMovement = true,
+                  disableCarMovement = true,
+                  disableMouse = false,
+                  disableCombat = true,
+                }, {
+                  animDict = "mp_take_money_mg",
+                  anim = "stand_cash_in_bag_loop",
+                  flags = 49,
+                }, {}, {}, function() -- Done
+                  local data = {}
+                  data.isHijacked = 0
+                  data.blocked = 1 -- Set to 1
+                  if atms[atmId].cashInside < earning then
+                    earning = atms[atmId].cashInside
+                  end
+                  data.cashInside = atms[atmId].cashInside - earning
+                  TriggerServerEvent('banking:server:GiveHijackCash', earning)
+                  TriggerServerEvent('rs-banking:server:UpdateATM', atmId, data)
+                  TriggerServerEvent('rs-banking:server:HijackTimer', atmId)
+                  StopAnimTask(GetPlayerPed(-1), "mp_take_money_mg", "stand_cash_in_bag_loop", 1.0)
+                end, function() -- Cancel
+                  ClearPedTasksImmediately(ped)
+                  StopAnimTask(GetPlayerPed(-1), "mp_take_money_mg", "stand_cash_in_bag_loop", 1.0)
+                  RSCore.Functions.Notify("Geannuleerd..", "error")
+                end)
+              end
+            end
+            
+            if IsControlJustPressed(1, Keys["E"]) and atms[atmId].isHijacked == 0 then
+              if (not IsInVehicle()) then
+                if bankOpen then
+                closeGui()
+                bankOpen = false
+                else
+                openGui()
+                bankOpen = true
+                end
+              end
+            end
+          else
+            if atms[atmId].cashInside > 0 then
+                DrawText3Ds(pos.x, pos.y, pos.z, 'Geldautomaat is stuk')
+            else
+                DrawText3Ds(pos.x, pos.y, pos.z, 'Geldautomaat is leeg')
+            end
+          end
+        end
+      end
+    end
+    
+    if not inRange then
+      Citizen.Wait(1500)
+    end
+  end
 end)
 
 -- Disable controls while GUI open
